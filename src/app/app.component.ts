a2c478b0b51465b7f2a1314d436b1c6a473c0415
Neamtu <neamtu.bc@yahoo.com>
2018-06-12 00:22:51 +0300

    -full map and 2 markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   lat: number = 46.7833002;
   lng: number = 23.4764293;
+  second_lat: number = 46.7843002;
+  second_lng: number = 22.4754293;
 }
